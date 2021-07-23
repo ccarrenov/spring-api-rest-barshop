2f57f9b9bb9f7ecfdeddb176e5261a5518dd057a
Cristian Carreño <cristian_c_v@hotmail.com>
2021-07-22 22:56:37 -0400

    ProcFile Heroku

--- src/main/java/com/barshop/app/App.java
+++ src/main/java/com/barshop/app/App.java
@@ -1,6 +1,5 @@
 package com.barshop.app;
 
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,16 +22,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class App {
 
     public static final String COUNTRY_TAG = "Country";
-
-    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
     
-    public static void main( String[] args ) {
-        LOGGER.info("engine: " +System.getenv("ENGINE_DB"));
-        LOGGER.info("conectordb: " +System.getenv("CONNECTOR_DB"));
-        LOGGER.info("dbdomain: " +System.getenv("DB_DOMAIN"));
-        LOGGER.info("engine: " +System.getenv("ENGINE_DB"));
-        LOGGER.info("loglvl: " +System.getenv("HB_LOG_LVL"));
-        
+    public static void main( String[] args ) {        
         SpringApplication.run(App.class, args);
     }
 
